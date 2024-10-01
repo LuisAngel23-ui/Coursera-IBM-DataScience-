cf8f1c354f6c84c269ce19aaa0e03ab1e1c5b274
LuisAngel23-ui <2ilaguilenc@gmail.com>
2024-10-01 13:08:13 -0600

    Add files via upload
    Update with sum of two numbers

--- Firstpython.py
+++ Firstpython.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 13:04:45 2024
+
+@author: 2ilag
+"""
+
+x = 1;
+y = 2;
+
+
+
 #Display the output
 
-print("New Python File")
+print("This is the result of the sum")
+print(x+y)
